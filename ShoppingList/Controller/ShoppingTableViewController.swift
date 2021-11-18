c6d2709e37be89b9c9690d026d1540823fe755bf
jmiltins <miltins.janis@gmail.com>
2021-11-18 22:57:42 +0200

    allert controllet with two text fields

---
+++ ShoppingList/Controller/ShoppingTableViewController.swift
@@ -0,0 +1,124 @@
+//
+//  ShoppingTableViewController.swift
+//  ShoppingList
+//
+//  Created by janis.miltins on 18/11/2021.
+//
+
+import UIKit
+import CoreData
+
+class ShoppingTableViewController: UITableViewController {
+    
+    var shopping = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    // loadData()
+    
+    //saveData()
+    
+    //deleteAllData()
+    
+    
+    @IBAction func addNewItem(_ sender: Any) {
+        
+        let allertController = UIAlertController(title: "Shopping Item", message: "What do you want to add?\nHow many? ", preferredStyle: .alert)
+        allertController.addTextField { textField in
+            print("textFieldItem: ", textField)
+        }
+        //allertController.
+        allertController.addTextField { textField2 in
+            print("textFieldCount: ", textField2)
+            //+add amount
+        }
+        // buttons
+        //addActionButton
+        let addActionButton = UIAlertAction(title: "Add", style: .default) { action in
+            let textField = allertController.textFields?.first
+            // append in array
+            self.shopping.append(textField!.text!)
+            //reload data after adding
+            self.tableView.reloadData()
+            
+        }
+        //cancel Button
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        //add two buttons
+        allertController.addAction(addActionButton)
+        allertController.addAction(cancelButton)
+        // present buttons
+        present(allertController, animated: true, completion: nil)
+        
+        
+    }
+    @IBAction func deleteAllItems(_ sender: Any) {
+    }
+    
+    
+    
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return shopping.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = shopping[indexPath.row]
+        return cell
+    }
+
+    // MARK: - Table view deligate
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
