94f89e3860d2f6479d2b314b8d6002559230e2f1
jmiltins <miltins.janis@gmail.com>
2021-11-19 01:29:37 +0200

    Add checkmark completed

--- ShoppingList/Controller/ShoppingTableViewController.swift
+++ ShoppingList/Controller/ShoppingTableViewController.swift
@@ -14,7 +14,7 @@ class ShoppingTableViewController: UITableViewController {
     var shopping = [Shopping]()
     
     var managedObjectContext: NSManagedObjectContext?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -70,7 +70,7 @@ class ShoppingTableViewController: UITableViewController {
             
             shop.setValue(textField?.text, forKey: "item")
             // append in array
-        //    self.shopping.append(textField!.text!)
+            //    self.shopping.append(textField!.text!)
             //reload data after adding
             self.saveData()
             self.tableView.reloadData()
@@ -91,68 +91,70 @@ class ShoppingTableViewController: UITableViewController {
     
     
     
-
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return shopping.count
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
+        
         // Configure the cell...
         //cell.textLabel?.text = shopping[indexPath.row]
         let shop = shopping[indexPath.row]
         cell.textLabel?.text = shop.value(forKey: "item") as? String
+        cell.accessoryType = shop.completed ? .checkmark : .none
         return cell
     }
-
+    
     // MARK: - Table view deligate
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            managedObjectContext?.delete(shopping[indexPath.row])
+        }
+        saveData()
     }
-    */
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        shopping[indexPath.row].completed = !shopping[indexPath.row].completed
+        saveData()
+    }
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
